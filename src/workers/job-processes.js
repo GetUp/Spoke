8b636d6a46f7b31a558bf65850dcc714dc6d1d9a
Benjamin Mort <benm@getup.org.au>
2017-10-26 13:59:36 +1100

    console on worker

--- src/workers/job-processes.js
+++ src/workers/job-processes.js
@@ -178,6 +178,7 @@ export async function assignmentQueuer() {
       if (activeAssignments) {
         await activeAssignments.map(async (assignment) => await messageQueuer(assignment))
       }
+      console.log('~~~~~~ Assginment Queuer', activeAssignments)
       await sleep(process.env.PREDICTIVE_ITERATION_PERIOD)
     } catch (ex) {
       log.error(ex)
